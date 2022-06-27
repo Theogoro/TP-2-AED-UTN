0a38abc44917f146de7bc3f64d6a62b25462a764
Theogoro <theoberta@gmail.com>
2022-06-26 23:13:26 -0300

    Delete test-10.py

--- test-10.py
+++
@@ -1,9 +0,0 @@
-def apuestas(pozo):
-    apuesta = int(input("Realice su apuesta (Debe ser un multiplo de 5):_$"))
-    while apuesta > pozo or apuesta < 1 or apuesta % 5 != 0:
-        apuesta = int(input("ERROR! La apuesta no es valida. $"))
-    print("SU APUESTA ES:", apuesta)
-
-    return apuesta
-
-apuestas(100)
